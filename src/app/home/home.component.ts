9d41e1b8af10a455bc16f14e85825f8336d699e7
Mrcsbda <macbedoya@gmail.com>
2023-03-21 17:24:36 -0500

    desktop

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,17 +11,20 @@ export class HomeComponent {
     {
       title: 'Brand Recognition',
       text: 'Boost your brand recognition with each click. Generic links dont mean a thing. Branded links help instil confidence in your content.',
-      img: '../../assets/home/icon-brand-recognition.svg'
+      img: '../../assets/home/icon-brand-recognition.svg',
+      class: 'card c1'
     },
     {
       title: 'Detailed Records',
       text: ' Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
-      img: '../../assets/home/icon-detailed-records.svg'
+      img: '../../assets/home/icon-detailed-records.svg',
+      class: 'card c2'
     },
     {
       title: ' Fully Customizable',
       text: 'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
-      img: '../../assets/home/icon-fully-customizable.svg'
+      img: '../../assets/home/icon-fully-customizable.svg',
+      class: 'card c3'
     },
   ]
   shortenLink(value:string) {
@@ -35,4 +38,5 @@ interface CardsInformation {
   title: string;
   text: string;
   img: string;
+  class: string;
 }
